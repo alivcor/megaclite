117046fbcaf12737ffda7e9ec3773d67f8e8f6e2
alivcor <abhinandandubey@live.com>
2018-04-07 15:32:07 -0400

    client side

---
+++ megaclite/static/index.js
@@ -0,0 +1,67 @@
+define([
+        'base/js/namespace',
+        'jquery',
+        'base/js/utils',
+        'base/js/dialog'
+], function(Jupyter, $, utils, dialog) {
+
+    function show_usage(){
+        var jh_username = utils.get_body_data('baseUrl').match(/.*\/user\/(.*)\//)[1]
+        var megacliteUrl = utils.url_path_join(utils.get_body_data('baseUrl'), 'megaclite/' + jh_username)
+        $.getJSON(megacliteUrl, function(data){
+                $("#maintoolbar-container").append("<span id=\"megaclite\"> &nbsp; <b> Memory Utilization : <span id=\"actual_val\">" + data['actual_val'] + "</span> MB </b></span>");
+
+            });
+        }
+
+        var warn_alerted = false;
+        var error_alerted = false;
+
+
+        function update_usage() {
+            var jh_username = utils.get_body_data('baseUrl').match(/.*\/user\/(.*)\//)[1]
+            var megacliteUrl = utils.url_path_join(utils.get_body_data('baseUrl'), 'megaclite/' + jh_username)
+            $.getJSON(megacliteUrl, function(data) {
+                if(data['actual_val'] == null){
+                    document.getElementById("actual_val").innerHTML = "Kernel appears to be dead.";
+                } else {
+                    document.getElementById("actual_val").innerHTML = data['actual_val'];
+
+                    if(data['zone'] == "N"){
+                        document.getElementById("megaclite").style = "color: #339900;";
+                    } else {
+                        if(data['zone'] == "W"){
+                            document.getElementById("megaclite").style = "color: #8B4513;";
+                            if(warn_alerted != true){
+                                dialog.modal(data);
+                                warn_alerted = true;
+
+                            }
+                        } else {
+                            document.getElementById("megaclite").style = "color: #d9534f;";
+                            if(error_alerted != true){
+                                dialog.modal(data);
+                                error_alerted = true;
+                            }
+
+                        }
+
+
+                    }
+
+
+                }
+
+
+
+            });
+
+        }
+
+
+
+
+
+
+
+})
